5e201d11b6e865da6881882a06a923e2c38172e6
llulima23-lang <llu.lima23@gmail.com>
2026-06-03 12:31:47 -0300

    Delete INICIAR_MONITORAMENTO.bat

--- INICIAR_MONITORAMENTO.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Muda para a pasta onde o arquivo .bat esta localizado
-cd /d "%~dp0"
-
-:: Inicia o watcher em modo silencioso (sem janela)
-start /b pythonw watcher.pyw
-
-:: Sai da janela de comando
-exit
